ec63c0d5a49961c3b0d052312a6e074cc9c3a068
Jan Dusza <duszajan@icloud.com>
2024-01-03 20:58:59 +0100

    dodałem schemat bazy danych

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "TripSplit"
 ]
 
 MIDDLEWARE = [
@@ -121,3 +122,8 @@ STATIC_URL = "static/"
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+import os
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_URL = '/media/'
+
