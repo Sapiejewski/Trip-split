b1de887251f256b8e94dcddbf2c98f56e835ae74
Sapiejewski <fsapiejewski01@gmail.com>
2024-01-06 16:19:20 +0100

    Navbar done and Card component done

---
+++ frontend/src/components/TripCard.js
@@ -0,0 +1,16 @@
+import React from "react"
+
+const TripCard = () => {
+	return (
+		<div className="flex flex-col items-start shadow py-[16px] w-1/3 mb-4">
+			<dic className="flex flex-col items-start justify-end">
+				<div>Narty we włoszech</div>
+			</dic>
+			<div>
+				<img src="" alt="dolomity"></img>
+			</div>
+		</div>
+	)
+}
+
+export default TripCard
