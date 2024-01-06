e719f3bdde309ad2b3e28a841bb67c2f0eacec21
Jan Dusza <84961730+J-Dusza@users.noreply.github.com>
2024-01-06 13:13:43 +0100

    dodałem schemat bazy danych (#13)

--- backend/TripSplit/admin.py
+++ backend/TripSplit/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Trip
+from .models import User
+from .models import Expense
+from .models import TripUser
+from .models import ExpenseUser
 
-# Register your models here.
+admin.site.register([Trip, User, Expense, TripUser, ExpenseUser])
