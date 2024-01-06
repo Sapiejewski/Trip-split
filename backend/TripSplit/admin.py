b035eab5c4e0125da53f5f08347487e0d06d2c71
ArekA <arekaffek4@gmail.com>
2024-01-06 15:29:59 +0100

    it works Co-authored-by: Piotr Bauer  <piotr.bauer.konto@gmail.com>

--- backend/TripSplit/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from .models import Trip
-from .models import User
-from .models import Expense
-from .models import TripUser
-from .models import ExpenseUser
-
-admin.site.register([Trip, User, Expense, TripUser, ExpenseUser])
