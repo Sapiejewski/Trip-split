cdf087fba6403d30d0c86b6a1b776a125ebb55d1
Sapiejewski <127155978+Sapiejewski@users.noreply.github.com>
2024-01-07 02:33:10 +0100

    Fixed AddNewTrip button css and fixed TripCard css (#22)

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,20 +1,39 @@
 import Footer from "./components/Footer.js"
 import Navbar from "./components/Navbar.js"
 import TripCard from "./components/TripCard.js"
+// import { BrowserRouter as Routes, Router, Route, Link } from "react-router-dom"
+import AddNewTrip from "./components/AddNewTrip.js"
 function App() {
 	return (
+		// <Router>
 		<div>
 			<Navbar />
-			<div className="flex flex-wrap justify-around px-96">
-				<TripCard />
-				<TripCard />
-				<TripCard />
-				<TripCard />
-				<TripCard />
-				<TripCard />
+			<div className="flex items-center justify-center mt-10 ">
+				<div className="flex flex-col items-center justify-between w-11/12  gap-y-5">
+					<div className="flex flex-row justify-between w-5/6 items-center">
+						<h1 className="text-xl font-bold">Moje Wyjazdy</h1>
+						{/* <Link to="/AddNewTrip"> */}
+						<button className="bg-[#B36AFB] text-[#FFFFFF] flex justify-center items-center py-[10px] shadow-md shadow-[#B36AFB]  rounded-lg   px-[20px]">
+							+ Nowy wyjazd
+						</button>
+						{/* </Link> */}
+					</div>
+					<div className="flex flex-wrap flex-row justify-center gap-x-5">
+						<TripCard />
+						<TripCard />
+						<TripCard />
+						<TripCard />
+						<TripCard />
+						<TripCard />
+					</div>
+				</div>
 			</div>
 			<Footer />
 		</div>
+		// 	<Routes>
+		// 		<Route path="/AddNewTrip" element={<AddNewTrip />} />
+		// 	</Routes>
+		// </Router>
 	)
 }
 
--- frontend/src/components/Content.js
+++ frontend/src/components/Content.js
@@ -1,7 +1,7 @@
 import React from "react"
 
 const Content = ({ children }) => {
-	return <div className="w-8/12">{children}</div>
+	return <div className="w-8/12 h-9/12">{children}</div>
 }
 
 export default Content
