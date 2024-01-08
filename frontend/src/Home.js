32711515cab3ff3276c16277374794bb74fdab45
Sapiejewski <127155978+Sapiejewski@users.noreply.github.com>
2024-01-08 17:08:48 +0100

    Frontend (#29)
    * adding new trip works
    
    * New TripUser in progress
    
    ---------
    
    Co-authored-by: Jan Dusza <84961730+J-Dusza@users.noreply.github.com>

--- frontend/src/Home.js
+++ frontend/src/Home.js
@@ -1,26 +1,35 @@
-import TripCard from "./components/TripCard.js"
+import TripCard from "./components/TripCard"
 import { Button } from "@nextui-org/react"
 import { BrowserRouter as Router, Route, Routes, Link } from "react-router-dom"
 import { useEffect, useState } from "react"
-
+import img from "./images/tripImages/img.jpg"
 const Home = () => {
 	const [data, setData] = useState([])
 	const [loading, setLoading] = useState(false)
+	// const images = [
+	// 	"./images/tripImages/airplaneWing.jpg",
+	// 	"./images/tripImages/DavidEm.jpg",
+	// 	"./images/tripImages/Góry.jpg",
+	// 	"./images/tripImages/GóryAless.jpg",
+	// 	"GóryKalen.jpg",
+	// 	"./images/tripImages/img.jpg",
+	// 	"./images/tripImages/LakeAaron.jpg",
+	// 	"./images/tripImages/TripBillard.jpg",
+	// 	"./images/tripImages/VacationPoints.jpg",
+	// ]
 	useEffect(() => {
 		setLoading(true)
 		fetch("http://localhost:8000/trip", {
 			headers: {
-				"Access-Control-Allow-Origin": "*",
 				"Content-Type": "application/json",
 			},
 		})
 			.then(res => res.json())
 			.then(data => {
-				console.log(data)
 				setData(data)
 			})
 		setLoading(false)
-	}, [data])
+	}, [])
 
 	return (
 		<div className="flex items-center justify-center mt-10">
@@ -32,20 +41,16 @@ const Home = () => {
 					</Link>
 				</div>
 				<div className="flex flex-wrap flex-row justify-center gap-x-5 gap-y-5 w-5/6">
-					{/* {data.map(item => (
+					{data.map(item => (
 						<TripCard
+							// img={images[item.id]}
+							trip_id={item.id}
 							name={item.name}
 							date_start={item.date_start}
 							date_end={item.date_end}
 							key={item.id}
 						/>
-					))} */}
-					<TripCard name="Narty we Włoszech" />
-					<TripCard />
-					<TripCard />
-					<TripCard />
-					<TripCard />
-					<TripCard />
+					))}
 				</div>
 			</div>
 		</div>
